08026bc8b19071f53f8cb645b85d20913815f11b
Maud Nalpas <maud.nalpas@gmail.com>
2016-11-01 14:41:09 +0100

    Refactor: templating, step #1

---
+++ planetsData.js
@@ -0,0 +1,21 @@
+var mercury = {
+  name: "Mercury",
+  color: "brown",
+  radius: "2.440",
+  position: "0 0 -400"
+};
+
+var venus = {
+  name: "Venus",
+  color: "yellow",
+  radius: "6.052",
+  position="280 0 -280"
+}
+
+var planets=[mercury, venus];
+
+/*function setPosition(){
+var distance = 400;
+}*/
+/*initialize.js: for each planet in planets: launch template*/
+/*coordinate: each planet's coordinates are the same as the neighbour but rotated*/
