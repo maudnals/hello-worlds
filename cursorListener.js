704c25ff59298c1a2e2c61cd3e44278a0d36c99b
Maud Nalpas <maud.nalpas@gmail.com>
2016-12-19 14:14:11 +0100

    Added cursor contextual update

--- cursorListener.js
+++ cursorListener.js
@@ -1,8 +1,13 @@
 AFRAME.registerComponent('cursor-listener', {
-  init: function(){
-    this.el.addEventListener('click', function(){ //this.el is the entity
-      //Click is like gaze
-      this.setAttribute('color', 'white'); // this is the sphere... ie the entity
-    });
-  }
+    init: function() {
+
+        this.el.addEventListener('click', function() { //this.el is the entity
+            //Click is like gaze
+            console.log(this.el);
+
+            this.setAttribute('color', 'white'); // this is the sphere... ie the entity
+
+            document.querySelector("a-cursor").setAttribute("color", "red");
+        });
+    }
 });
