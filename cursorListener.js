8819b18baea38e5aba7c9a85ea34a200ed44a17c
Maud Nalpas <maud.nalpas@gmail.com>
2016-12-19 15:24:56 +0100

    Added visual feedback that planet is selected

--- cursorListener.js
+++ cursorListener.js
@@ -1,12 +1,24 @@
 AFRAME.registerComponent('cursor-listener', {
     init: function() {
 
-        this.el.addEventListener('click', function() { //this.el is the entity
+        this.el.addEventListener('click', function() {
+            //this.el is the entity
             //Click is like gaze
             console.log(this.el);
-
             this.setAttribute('color', 'white'); // this is the sphere... ie the entity
+            //console.log(this.getAttrradius);
+            // var newRadius = this.getAttribute("radius") * 2;
 
+
+
+            // Todo:
+            // Add good cursor transitions
+            // Add good object transitions when selected
+            // Remove transitions when relevant 
+            // Display text on click
+            var newRadius = 120;
+            console.log(newRadius);
+            this.setAttribute('radius', newRadius);
             document.querySelector("a-cursor").setAttribute("color", "red");
         });
     }
