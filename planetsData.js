e69029b6e2a5f1d88105957695afd194ce3d205f
Maud Nalpas <maud.nalpas@gmail.com>
2016-12-20 18:58:01 +0100

    Completed planet data in proper object format

--- planetsData.js
+++ planetsData.js
@@ -1,36 +1,59 @@
 var planets = {
     'mercury': {
         id: 'mercury',
-        color: 'slategrey'
+        color: 'slategrey',
+        position: '0 180 -400',
+        radius: '2.440',
+        texture: 'img/2k_mercury.jpg'
     },
     'venus': {
         id: 'venus',
-       	color: 'darkorange'
-        //color: 'whitesmoke'
+        color: 'darkorange',
+        position: '280 180 -280',
+        radius: '6.052',
+        texture: 'img/2k_venus_surface.jpg'
     },
     'earth': {
         id: 'earth',
-        color: '#0054FF'
+        color: '#0054FF',
+        position: '400 180 0',
+        radius: '6.378',
+        texture: 'img/2k_earth_daymap.jpg'
     },
     'mars': {
         id: 'mars',
-        color: 'orangered'
+        color: 'orangered',
+        position: '280 180 280',
+        radius: '3.397',
+        texture: 'img/2k_mars.jpg'
     },
     'jupiter': {
         id: 'jupiter',
-        color: 'burlywood'
+        color: 'burlywood',
+        position: '0 180 400',
+        radius: '71.492',
+        texture: 'img/Txtr-Jupiter.jpg'
     },
     'saturn': {
         id: 'saturn',
-        color: 'wheat'
+        color: 'wheat',
+        position: '-280 180 -280',
+        radius: '60.368',
+        texture: 'img/2k_saturn.jpg'
     },
     'uranus': {
         id: 'uranus',
-        color: 'powderblue'
+        color: 'powderblue',
+        position: '-400 180 0',
+        radius: '25.559',
+        texture: 'img/2k_uranus.jpg'
     },
     'neptune': {
         id: 'neptune',
-        color: 'royalblue'
+        color: 'royalblue',
+        position: '-280 180 280',
+        radius: '24.766',
+        texture: 'img/2k_neptune.jpg'
     }
 };
 
