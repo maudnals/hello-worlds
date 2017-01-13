367d266a7e9a43d83d856095ffade9624d0e3dae
Maud Nalpas <maud.nalpas@gmail.com>
2017-01-13 21:06:05 +0100

    Display data: prep some more space by making planet smaller

--- js/const.js
+++ js/const.js
@@ -1,6 +1,6 @@
 const textPosition = '3 1 -6';
 const frontPlanetPosition = '0 20 -40';
-const radiusFocus = 8;
+const radiusFocus = 6;
 const none = 'none';
 
 // const initCameraPosition = '0 2000 0';
