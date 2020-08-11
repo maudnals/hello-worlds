2fa4272d4d4ac555a048d15e7934b66c371f0e60
maudnals <maud.nalpas@gmail.com>
2020-08-11 22:16:30 +0200

    refactor: cleaned

--- js/app.js
+++ js/app.js
@@ -1,39 +1,9 @@
-// let myCursor;
-cursorListener.init();
-let voices;
-let idle = "idle";
-let over = "over";
-let visit = "visit";
+cursorListener.init()
+let idle = 'idle'
+let visit = 'visit'
 
-document.addEventListener('DOMContentLoaded', function() {
-    //cursor = document.getElementById("cursor");
-    // myCursor = document.getElementById("cursor");
-    planetGenerator.init();
-    renderer.init();
-    utils.init();
-
-    // cursor.addEventListener('mouseenter', function(event) {
-    //     console.log("mouseenter", event.detail.intersectedEl.id);
-    //     if (event.detail.intersectedEl.className === "planet-sphere") {
-    //     	document.getElementById('leaveButtonPlane').setAttribute("material", material="color: mediumspringgreen; opacity: 0.3;");
-    //         let planetId = event.detail.intersectedEl.id;
-    //         console.log("display name");
-    //         renderer.updateViewFuse(planetId);
-    //     }
-    // });
-
-    // cursor.addEventListener('mouseenter', function(event) {
-    //     console.log("mouseenter", event.detail.intersectedEl.id);
-    //     if (event.detail.intersectedEl.id === "leaveButtonPlane") {
-    //         event.detail.intersectedEl.setAttribute("material", material="color: mediumspringgreen; opacity: 0.55;");
-    //         //renderer.updateViewFuse(planetId);
-    //     }
-    // });
-
-    // cursor.addEventListener('mouseleave', ... // Not needed
-
-});
-
-window.speechSynthesis.onvoiceschanged = function() {
-    voices = window.speechSynthesis.getVoices();
-}
+document.addEventListener('DOMContentLoaded', function () {
+  planetGenerator.init()
+  renderer.init()
+  utils.init()
+})
