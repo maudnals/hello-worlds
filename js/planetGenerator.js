8b0e4656cfaf99955ff5ce949cd8d742867b0f75
maudnals <maud.nalpas@gmail.com>
2020-08-11 22:32:12 +0200

    style: prettier

--- js/planetGenerator.js
+++ js/planetGenerator.js
@@ -4,26 +4,26 @@ PLANET GENERATOR
 
 let planetGenerator = (function () {
   function init() {
-    let planetEntities = utils.getAllPlanetElements();
-    planetEntities.forEach((p) => {
-      const id = p.getAttribute("id");
-      const { radius, defaultPosition, texture, rotationPeriod } = planets[id];
-      p.setAttribute("position", defaultPosition);
-      p.setAttribute("material", "src: url(" + texture + ")");
-      p.setAttribute("radius", radius);
-      p.setAttribute("animation", {
-        property: "rotation",
-        to: "0 360 0",
+    let planetElements = utils.getAllPlanetElements()
+    planetElements.forEach((p) => {
+      const id = p.getAttribute('id')
+      const { radius, defaultPosition, texture, rotationPeriod } = planets[id]
+      p.setAttribute('position', defaultPosition)
+      p.setAttribute('material', 'src: url(' + texture + ')')
+      p.setAttribute('radius', radius)
+      p.setAttribute('animation', {
+        property: 'rotation',
+        to: '0 360 0',
         dur: rotationPeriod,
-        easing: "linear",
+        easing: 'linear',
         loop: true,
-        repeat: "indefinite",
-        fill: "forwards",
-      });
-    });
+        repeat: 'indefinite',
+        fill: 'forwards'
+      })
+    })
   }
 
   return {
-    init: init,
-  };
-})();
+    init: init
+  }
+})()
--- js/vectorHelper.js
+++ js/vectorHelper.js
@@ -2,43 +2,39 @@
 VECTOR HELPER
 ------------------ */
 
-let vectorHelper = (function() {
+let vectorHelper = (function () {
+  function degToRad(angle) {
+    // 360 deg = 2Pi radians
+    return (angle * 2 * Math.PI) / 360
+  }
 
-    function degToRad(angle) {
-        // 360 deg = 2Pi radians
-        return angle * 2 * Math.PI / 360;
-    }
+  function getVectorFromPosition(position) {
+    let arr = position.split(' ')
+    let x = parseFloat(arr[0])
+    let y = parseFloat(arr[1])
+    let z = parseFloat(arr[2])
+    return new THREE.Vector3(x, y, z)
+  }
 
-    function getVectorFromPosition(position) {
-        let arr = position.split(" ");
-        let x = parseFloat(arr[0]);
-        let y = parseFloat(arr[1]);
-        let z = parseFloat(arr[2]);
-        return new THREE.Vector3(x, y, z);
-    }
+  function getPositionFromVector(vector) {
+    return vector.x + ' ' + vector.y + ' ' + vector.z
+  }
 
-    function getPositionFromVector(vector) {
-        return vector.x + ' ' + vector.y + ' ' + vector.z;
-    }
+  function generateNeighborPosition(position, yAngle) {
+    const yAxis = new THREE.Vector3(0, 1, 0)
+    const positionVector = getVectorFromPosition(position)
+    const neighborPositionVector = positionVector.applyAxisAngle(
+      yAxis,
+      degToRad(yAngle)
+    )
+    const neighborPosition = getPositionFromVector(neighborPositionVector)
+    return neighborPosition
+  }
 
-    function generateNeighborPosition(position, yAngle) {
-        // + , zAngle
-        //let angleRad = angle.y * 2 * Math.PI / 360;
-        let yAxis = new THREE.Vector3(0, 1, 0);
-        let positionVector = getVectorFromPosition(position);
-        let neighborPositionVector = positionVector.applyAxisAngle(yAxis, degToRad(yAngle));
-        let neighborPosition = getPositionFromVector(neighborPositionVector);
-
-        return neighborPosition;
-    }
-
-    return {
-        generateNeighborPosition: generateNeighborPosition,
-        getPositionFromVector: getPositionFromVector
-    }
-})();
-
-
+  return {
+    generateNeighborPosition: generateNeighborPosition,
+    getPositionFromVector: getPositionFromVector
+  }
+})()
 
 // camera should rotate along y only
-
