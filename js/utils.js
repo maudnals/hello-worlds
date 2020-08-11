077cdcbb01c7a40101baf9364306974e8f85fda9
maudnals <maud.nalpas@gmail.com>
2020-08-11 23:03:02 +0200

    refactor: arrow function

--- js/utils.js
+++ js/utils.js
@@ -6,29 +6,24 @@ let utils = (function () {
   function init() {
     leaveButton = document.querySelector('#leaveButton')
   }
-
   function getAllPlanetElements() {
     return document.querySelectorAll('.planet-sphere')
   }
 
   function show(element) {
-    element.setAttribute('visible', 'true')
+    element.setAttribute('visible', true)
   }
 
   function hide(element) {
-    element.setAttribute('visible', 'false')
+    element.setAttribute('visible', false)
   }
 
   function showAll(elements) {
-    elements.forEach(function (e) {
-      show(e)
-    })
+    elements.forEach((e) => show(e))
   }
 
   function hideAll(elements) {
-    elements.forEach(function (e) {
-      hide(e)
-    })
+    elements.forEach((e) => hide(e))
   }
 
   return {
