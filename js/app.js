3c6832e8c102b7ecfbe38353007f759fdca12c71
maudnals <maud.nalpas@gmail.com>
2020-08-11 22:32:24 +0200

    style: arrow function

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ cursorListener.init()
 let idle = 'idle'
 let visit = 'visit'
 
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
   planetGenerator.init()
   renderer.init()
   utils.init()
